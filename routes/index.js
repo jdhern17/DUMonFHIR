dc937fad8ebbfc01193a9b4aed07c40a766cd18c
jdhgit <17481018+jdhern17@users.noreply.github.com>
2020-02-07 22:27:40 -0500

    removing unnecessary code from Books page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const path = require("path");
+
+// creates a new router object
 const router = require("express").Router();
+
 const apiRoutes = require("./api");
 
 // API Routes
@@ -7,7 +10,7 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
 module.exports = router;
